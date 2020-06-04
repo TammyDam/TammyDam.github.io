3a1f66604638d5d16b1a51ece1a133862cf9ea9c
TammyDam <tammydam1999@gmail.com>
2020-06-04 14:19:53 -0700

    try

---
+++ testing/p.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php
+echo $_POST["name"]; ?><br>
+Your 
+?>
+
+</body>
+</html>
---
+++ testing/phpfilters/i.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Filters in PHP</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/jquery.css">
+        <script type="text/javascript" src="js/jquery-1.12.4.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.js"></script>
+        <script type="text/javascript">
+        $( function(){
+            $( "#from" ).datepicker();
+        } );
+
+        $( function(){
+            $( "#to" ).datepicker();
+        } );
+    </script>
+</head>
+<body>
+    <div class="container"></div>
+    </body>
+    </html>
